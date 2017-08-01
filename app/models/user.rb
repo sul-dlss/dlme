ba509c968dcfe0a02b382fcbf25b86acb6b16be5
Chris Beer <chris@cbeer.info>
2017-08-01 15:54:42 -0700

    Run the blacklight:user generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Connects this user object to Blacklights Bookmarks.
+  include Blacklight::User
   include Spotlight::User # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
