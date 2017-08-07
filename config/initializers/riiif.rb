1e1dca34ef3519f0f329c48f887c302365ac5acf
Chris Beer <chris@cbeer.info>
2017-08-07 14:21:47 -0700

    Use Riiif::HTTPFileResolver for fetching and rendering S3-hosted resources

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,5 +1,14 @@
-Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
+if Settings.s3.upload_bucket
+  Riiif::Image.file_resolver = Riiif::HTTPFileResolver.new
+  resolver.id_to_uri = lambda do |id|
+    aws_file = Spotlight::FeaturedImage.find(id).image.file
+    raise Riiif::ImageNotFoundError, "unable to find file for #{id}" if aws_file.nil?
 
+    aws_file.file.presigned_url('GET')
+  end
+else
+  Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
+end
 # Riiif::Image.authorization_service = IIIFAuthorizationService
 
 # Riiif.not_found_image = 'app/assets/images/us_404.svg'
