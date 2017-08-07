bce14b717760cbcd57ef30b137330d339f26c6bd
Justin Coyne <jcoyne@justincoyne.com>
2017-08-07 11:26:42 -0500

    Validate JSON

--- app/models/dlme_json.rb
+++ app/models/dlme_json.rb
@@ -3,8 +3,19 @@
 # Model to represent the intermediate representation (in JSON) of a DLME object
 class DlmeJson < Spotlight::Resource
   self.document_builder_class = DlmeJsonResourceBuilder
+  validate :valid_json?
 
+  # @raise [JSON::ParserError] if the json is not parsable
   def json
     @json ||= JSON.parse(data[:json])
+  end
+
+  private
+
+  def valid_json?
+    json
+    true
+  rescue JSON::ParserError
+    errors.add(:json, 'Invalid JSON')
   end
 end
