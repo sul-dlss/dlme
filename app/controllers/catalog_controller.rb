1e9445045c0ba135fc5141744d396dcb56682d10
Chris Beer <chris@cbeer.info>
2017-08-08 07:55:04 -0700

    Copy index styling from exhibits to align thumbnails a little better

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -9,6 +9,7 @@ class CatalogController < ApplicationController
     config.show.oembed_field = :oembed_url_ssm
     config.show.partials.insert(1, :oembed)
 
+    config.view.list.partials = %i[thumbnail index_header index]
     config.view.gallery.partials = %i[index_header index]
     config.view.masonry.partials = [:index]
     config.view.slideshow.partials = [:index]
