dcab205e36b9ba2d100b2b3fbca0a193f27151e2
Christina Harlow <cmharlow@gmail.com>
2017-08-15 10:22:56 -0700

    using cbeer work for nested resource IR output, isolated IIIF macros

--- lib/traject/macros/iiif.rb
+++ lib/traject/macros/iiif.rb
@@ -4,69 +4,70 @@ module Macros
   # Macros for extracting Stanford Specific MODS values from Nokogiri documents
   module Stanford
     def grab_sul_iiif_links(manifest)
-      result = Net::HTTP.get(URI.parse(manifest))
-      process_iiif_json(result)
+      manifest = Net::HTTP.get(URI.parse(manifest))
+      JSON.parse(manifest)
     rescue
       nil
     end
 
-    def process_iiif_thumbnail(result)
-      resp = JSON.parse(result)
-      resp['thumbnail']['@id']
+    def process_iiif_thumbnail(manifest_json)
+      manifest_json['thumbnail']['@id']
     rescue JSON::ParserError
       nil
     end
 
-    def process_iiif_thumbnail_service(result)
-      resp = JSON.parse(result)
-      resp['thumbnail']['service']['@id']
+    def process_iiif_thumbnail_service(manifest_json)
+      manifest_json['thumbnail']['service']['@id']
     rescue JSON::ParserError
       nil
     end
 
-    def process_iiif_thumbnail_protocol(result)
-      resp = JSON.parse(result)
-      resp['thumbnail']['service']['profile']
+    def process_iiif_thumbnail_protocol(manifest_json)
+      manifest_json['thumbnail']['service']['profile']
     rescue JSON::ParserError
       nil
     end
 
-    def process_iiif_thumbnail_conforms_to(result)
-      resp = JSON.parse(result)
-      iiif_service_conforms_to(resp['thumbnail']['service']['profile'])
+    def process_iiif_thumbnail_conforms_to(manifest_json)
+      iiif_service_conforms_to(manifest_json['thumbnail']['service']['profile'])
     rescue JSON::ParserError
       nil
     end
 
-    def process_iiif_sequences(result)
-      resp = JSON.parse(result)
-      iiif_service(resp['sequences'])
-      iiif_service_conforms_to(resp)
+    def process_iiif_sequences(manifest_json)
+      manifest_json['sequences'].first['canvases'].first['images'].first['resource']['@id']
     rescue JSON::ParserError
       nil
     end
 
-    def iiif_service(sequences)
-      to_field 'wr_has_service' do |accumulator|
-        sequences.each do |sequence|
-          sequence['canvases'].each do |canvas|
-            canvas['images'].each do |image|
-              accumulator << image['resource']['service']['id']
-            end
-          end
-        end
-      end
+    def process_iiif_sequences_service_id(manifest_json)
+      manifest_json['sequences'].first['canvases'].first['images'].first['resource']['service']['@id']
+    rescue JSON::ParserError
+      nil
     end
 
-    def iiif_service_conforms_to(thumbnail_service_profile)
+    def process_iiif_sequences_service_profile(manifest_json)
+      manifest_json['sequences'].first['canvases'].first['images'].first['resource']['service']['profile']
+    rescue JSON::ParserError
+      nil
+    end
+
+    def process_iiif_sequences_conforms_to(manifest_json)
+      profile = manifest_json['sequences'].first['canvases'].first['images'].first['resource']['service']['profile']
+      iiif_service_conforms_to(profile)
+    rescue JSON::ParserError
+      nil
+    end
+
+    def iiif_service_conforms_to(service_profile)
       # Using the thumbnail service profile for now
-      if thumbnail_service_profile.include? 'http://iiif.io/api/image/'
+      if service_profile.include? 'http://iiif.io/api/image/'
         literal('http://iiif.io/api/image/')
-      elsif thumbnail_service_profile.include? 'http://iiif.io/api/auth/'
+      elsif service_profile.include? 'http://iiif.io/api/auth/'
         literal('http://iiif.io/api/auth/')
-      elsif thumbnail_service_profile.include? 'http://iiif.io/api/presentation/'
+      elsif service_profile.include? 'http://iiif.io/api/presentation/'
         literal('http://iiif.io/api/presentation/')
-      elsif thumbnail_service_profile.include? 'http://iiif.io/api/search/'
+      elsif service_profile.include? 'http://iiif.io/api/search/'
         literal('http://iiif.io/api/search/')
       end
     end
