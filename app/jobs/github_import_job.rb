0a01b8cefc6cea4d13b267571a83713c96ce6069
Justin Coyne <jcoyne@justincoyne.com>
2017-08-15 12:11:48 -0500

    Use the GH access token from Settings rather than parameter passing

--- app/jobs/github_import_job.rb
+++ app/jobs/github_import_job.rb
@@ -6,32 +6,19 @@ require 'github_importer'
 class GithubImportJob < ApplicationJob
   queue_as :default
 
-  def perform(github_token)
-    importer(github_token).import(import_directory) do |filename, content|
+  def perform
+    importer.import(import_directory) do |filename, content|
       process_file(filename, content)
     end
   end
 
   private
 
-  def importer(github_token)
+  def github_token
+    Settings.import.access_token
+  end
+
+  def importer
     GithubImporter.new(github_token, Settings.import.repo)
   end
 end
-
-# Patch the logger to prevent the github access_token from being logged
-module SilentArgsLogger
-  private
-
-  def args_info(job)
-    return super unless job.class < GithubImportJob
-    args_excluding_first(job)
-  end
-
-  def args_excluding_first(job)
-    ' with arguments: ***REDACTED*** ' +
-      job.arguments[1..-1].map { |arg| format(arg).inspect }.join(', ')
-  end
-end
-
-ActiveJob::Logging::LogSubscriber.prepend SilentArgsLogger
--- spec/jobs/stanford_import_job_spec.rb
+++ spec/jobs/stanford_import_job_spec.rb
@@ -6,13 +6,14 @@ RSpec.describe StanfordImportJob, type: :job do
   let(:importer) { double }
 
   before do
+    allow(Settings.import).to receive(:access_token).and_return('TOKEN')
     allow(GithubImporter).to receive(:new).and_return(importer)
     allow(importer).to receive(:import).and_yield('foo.mods', 'bar')
     allow(ModsTransformJob).to receive(:perform_later)
   end
 
   it 'imports files' do
-    described_class.perform_now('TOKEN')
+    described_class.perform_now
 
     expect(GithubImporter).to have_received(:new)
       .with('TOKEN', 'waynegraham/dlme-metadata')
