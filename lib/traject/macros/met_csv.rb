b193ef2300b0f43797b5fb87977281f66e34e55c
Aaron Collier <aaron.collier@stanford.edu>
2017-08-17 20:04:19 -0700

    Add met_cvs config and macro. Edit fixture to add non-blank creator test

---
+++ lib/traject/macros/met_csv.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+module Macros
+  # Macros for extracting values from CSV rows
+  module MetCsv
+    # @param header_or_index [String] the field header or index to accumulate
+
+    DISPLAY_NAME = 'Artist Display Name'
+    SUFFIX = 'Artist Suffix'
+    BEGIN_DATE = 'Artist Begin Date'
+    END_DATE = 'Artist End Date'
+    OBJECT_BEGIN_DATE = 'Object Begin Date'
+    OBJECT_END_DATE = 'Object End Date'
+    ROLE = 'Artist Role'
+    BIO = 'Artist Display Bio'
+    CLASSIFICATION = 'Classification'
+    PUBLIC_DOMAIN = 'Is Public Domain'
+    DEPARTMENT = 'Department'
+    REPOSITORY = 'Repository'
+
+    def generate_creator
+      lambda do |row, accumulator, _context|
+        accumulator << [row[DISPLAY_NAME], row[SUFFIX], artist_role_bio(row)].select(&:present?).join(', ')
+      end
+    end
+
+    def artist_date_range(row)
+      @artist_date_range ||= [row[BEGIN_DATE], row[END_DATE]].select(&:present?).join(' - ')
+    end
+
+    def artist_role_bio(row)
+      @artist_role_bio ||= "#{artist_date_range(row)} (#{[row[ROLE], row[BIO]].select(&:present?).join(' ; ')})"
+    end
+
+    def generate_object_date
+      lambda do |row, accumulator, _context|
+        object_date = row[OBJECT_BEGIN_DATE] unless row[OBJECT_BEGIN_DATE].to_s.empty?
+        object_date = "#{object_date} - #{row[OBJECT_END_DATE]}" unless row[OBJECT_END_DATE].to_s.empty?
+        accumulator << object_date
+      end
+    end
+
+    def edm_type
+      lambda do |row, accumulator, _context|
+        accumulator << '3D' unless row[CLASSIFICATION].empty?
+      end
+    end
+
+    def type_if_classification
+      lambda do |row, accumulator, _context|
+        accumulator << 'three dimensional object' unless row[CLASSIFICATION].empty?
+      end
+    end
+
+    def public_domain
+      lambda do |row, accumulator, _context|
+        accumulator << 'Public Domain' if row[PUBLIC_DOMAIN] == 'True'
+        accumulator << 'Not Public Domain' if row[PUBLIC_DOMAIN] == 'False'
+      end
+    end
+
+    def data_provider
+      lambda do |row, accumulator, _context|
+        accumulator << [row['Department'], row['Repository']].select(&:present?).join(', ')
+      end
+    end
+  end
+end
