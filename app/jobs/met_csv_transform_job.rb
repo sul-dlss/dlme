3369efc225d5c35b3806eeed220e792fc7553efa
Aaron Collier <aaron.collier@stanford.edu>
2017-08-18 10:26:44 -0700

    Add MetCsvImportJob and MetCsvTransformJob

---
+++ app/jobs/met_csv_transform_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Transform METS flavored CSV data into the IR and index it
+class MetCsvTransformJob < ApplicationJob
+  queue_as :default
+
+  def perform(identifier, met_csv)
+    indexer = Traject::Indexer.new('identifier' => identifier)
+    indexer.load_config_file('config/traject.rb')
+    indexer.load_config_file('lib/traject/met_csv_config.rb')
+    indexer.process(met_csv)
+  end
+end
---
+++ spec/jobs/met_csv_transform_job_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MetCsvTransformJob, type: :job do
+  let(:indexer) { instance_double(Traject::Indexer, load_config_file: true, process: true) }
+
+  before do
+    allow(Traject::Indexer).to receive(:new).and_return(indexer)
+  end
+
+  it 'imports files' do
+    described_class.perform_now('IDENTIFIER', 'METCSV')
+
+    expect(indexer).to have_received(:load_config_file)
+      .with('lib/traject/met_csv_config.rb')
+
+    expect(indexer).to have_received(:process)
+      .with('METCSV')
+  end
+end
