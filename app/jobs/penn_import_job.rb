a0e64c8bfdae3ff073d45f8017ee9e90fe055a69
Justin Coyne <jcoyne@justincoyne.com>
2017-08-18 20:16:56 -0500

    Remove individual github importer classes
    The differences can be expressed as parameters.
    
    Fixes #115
    Fixes #112

--- app/jobs/penn_import_job.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Imports the TEI files from the github directory configured in +Settings.import.directory.penn+
-class PennImportJob < GithubImportJob
-  self.import_directory = Settings.import.directory.penn
-  self.pipeline = Pipeline.for('penn_tei')
-end
