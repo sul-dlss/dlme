f9787107f6e1032f38e39d8668d4206a8bc6179b
Justin Coyne <jcoyne@justincoyne.com>
2017-08-23 14:19:15 -0500

    Import Met thumbnails. Fixes #101

--- spec/import/met_csv_transform_spec.rb
+++ spec/import/met_csv_transform_spec.rb
@@ -4,21 +4,34 @@ require 'rails_helper'
 
 RSpec.describe 'Transforming MET CSV files' do
   let(:indexer) do
-    Traject::Indexer.new('exhibit_slug' => slug).tap do |i|
+    Traject::Indexer.new(provide).tap do |i|
       i.load_config_file('lib/traject/met_csv_config.rb')
     end
   end
   let(:fixture_file_path) { File.join(fixture_path, 'csv/met.csv') }
-  let(:data) { File.open(fixture_file_path).read }
+  let(:data) do
+    # Read the first 4 lines (1 for headers and 3 for the first record)
+    File.open(fixture_file_path) { |f| f.readline + f.readline + f.readline + f.readline }
+  end
   let(:exhibit) { create(:exhibit) }
   let(:slug) { exhibit.slug }
 
+  let(:provide) do
+    { 'exhibit_slug' => slug,
+      'writer_class_name' => 'Traject::JsonWriter' }
+  end
+  let(:writer) { instance_double Traject::JsonWriter, put: '' }
+
   before do
-    allow(CreateResourceJob).to receive(:perform_later)
+    allow(Traject::JsonWriter).to receive(:new).and_return(writer)
   end
 
   it 'does the transform' do
     indexer.process(data)
-    expect(CreateResourceJob).to have_received(:perform_later).exactly(3).times
+    expect(writer).to have_received(:put) do |context|
+      dlme = context.output_hash
+      expect(dlme['agg_preview'])
+        .to eq [{ 'wr_id' => ['http://images.metmuseum.org/CRDImages/an/web-large/ss74_51_4366gp.JPG'] }]
+    end
   end
 end
