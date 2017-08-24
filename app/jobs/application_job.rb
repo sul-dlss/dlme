e7a35d70c13ab0e309aa0c7b206d0fbbef81e3a5
Chris Beer <chris@cbeer.info>
2017-08-23 17:55:32 -0700

    Explicitly enqueue jobs on the default queue; implicit queueing doesn't respect the queue name prefix

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
+  # explicitly set the queue name in order to trigger
+  # rails queue name prefix behavior.
+  queue_as ActiveJob::Base.default_queue_name
 end
