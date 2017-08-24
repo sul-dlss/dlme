22f3506fee770b07ec22ceaab9b2266f75243963
Justin Coyne <jcoyne@justincoyne.com>
2017-08-24 11:21:20 -0500

    Allow harvests to be started in the UI.

---
+++ spec/controllers/harvests_controller_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HarvestsController do
+  let(:admin) { create(:site_admin) }
+
+  before do
+    sign_in admin
+  end
+
+  describe 'GET index' do
+    it 'is successful' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe 'POST create' do
+    before do
+      allow(ImportJob).to receive(:perform_later)
+    end
+    it 'is successful' do
+      post :create
+      expect(response).to redirect_to harvests_path
+      expect(ImportJob).to have_received(:perform_later).with(Harvest)
+    end
+  end
+end
