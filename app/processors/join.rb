1fa0e711ec639703da19e51d3435d86a12b85375
Justin Coyne <jcoyne@justincoyne.com>
2017-08-29 13:31:34 -0500

    Join without html tags
    This becomes useful once we have
    https://github.com/projectblacklight/blacklight/pull/1756
    
    Ref #202

--- app/processors/join.rb
+++ app/processors/join.rb
@@ -5,9 +5,18 @@ class Join < Blacklight::Rendering::AbstractStep
   include ActionView::Helpers::TextHelper
 
   def render
-    # rubocop:disable Rails/OutputSafety
-    joiner = config.join_with || '<br>'.html_safe
-    # rubocop:enable Rails/OutputSafety
     next_step(safe_join(values, joiner))
+  end
+
+  private
+
+  def joiner
+    config.join_with || default_joiner
+  end
+
+  def default_joiner
+    # rubocop:disable Rails/OutputSafety
+    options[:no_html] ? '; ' : '<br>'.html_safe
+    # rubocop:enable Rails/OutputSafety
   end
 end
