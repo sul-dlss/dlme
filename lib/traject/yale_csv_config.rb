27865a89c88f3218c52c709735df6f5e910e0195
Wayne Graham <wgraham@clir.org>
2017-09-02 10:42:45 -0400

    Fix Yale configuration issues
    Adds proper configuration and tests for the Yale Medical records for the DMLE.
    
    Closes #215

--- lib/traject/yale_csv_config.rb
+++ lib/traject/yale_csv_config.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # Yale CSV Mapping Configuration
-
 require_relative 'csv_reader'
 require_relative 'dlme_json_resource_writer'
 require_relative 'macros/dlme'
@@ -14,26 +13,34 @@ settings do
   provide 'reader_class_name', 'CsvReader'
 end
 
-to_field 'agg_provider', provider
-
-to_field 'id', column('OrbisBibID')
-to_field 'cho_id', column('OrbisBibID')
-
-to_field 'cho_provenance', column('Place of origin')
-to_field 'cho_creator', column('Creator', split: '|')
-to_field 'cho_title', column('Title', split: '|')
-to_field 'cho_alternative_title', column('Varient Titles', split: '|')
-to_field 'cho_publisher', column('Publisher')
-to_field 'edm:dateProvider', column('Date, created')
-to_field 'cho_description', column('Physical description')
-to_field 'cho_language', column('Language')
-to_field 'cho_description', column('Notes')
-to_field 'cho_description', column('Abstract')
-to_field 'cho_provenance', column('Associated Names')
-to_field 'cho_subject', column('Subject, topic', split: '|')
-to_field 'cho_edm_type', column('Type of resource')
-to_field 'cho_is_part_of', column('Yale Collection')
-to_field 'wr_dc_rights', column('Rights')
-to_field 'cho_provenance', column('part of, group')
-to_field 'agg_is_shown_by', column('Handle')
-to_field 'agg_is_shown_at', column('Handle')
+to_field 'agg_provider',     provider
+to_field 'id',               normalize_prefixed_id('OrbisBibID')
+to_field 'cho_identifier',   column('OrbisBibID', trim: true)
+to_field 'cho_provenance',   column('Place of origin', trim: true)
+to_field 'cho_creator',      column('Creator', split: '|', trim: true)
+to_field 'cho_title',        column('Title', split: '|', trim: true)
+to_field 'cho_alternative',  column('Variant Titles', split: '|', trim: true)
+to_field 'cho_publisher',    column('Publisher', trim: true)
+to_field 'edm:dateProvider', column('Date, created', trim: true)
+to_field 'cho_description',  column('Physical description', trim: true)
+to_field 'cho_language',     column('Language', trim: true)
+to_field 'cho_description',  column('Notes', split: '|', trim: true)
+to_field 'cho_description',  column('Abstract', trim: true)
+to_field 'cho_provenance',   column('Associated Names', trim: true)
+to_field 'cho_subject',      column('Subject, topic', split: '|', trim: true)
+to_field 'cho_edm_type',     column('Type of resource', trim: true)
+to_field 'cho_is_part_of',   column('Yale Collection', trim: true)
+to_field 'wr_dc_rights',     column('Rights', trim: true)
+to_field 'cho_provenance',   column('part of, group', trim: true)
+to_field 'agg_is_shown_at' do |_record, accumulator, context|
+  accumulator << transform_values(
+    context,
+    'wr_id' => [column('Handle', trim: true)]
+  )
+end
+to_field 'agg_is_shown_by' do |_record, accumulator, context|
+  accumulator << transform_values(
+    context,
+    'wr_id' => [column('Handle', trim: true)]
+  )
+end
--- spec/import/yale_transform_spec.rb
+++ spec/import/yale_transform_spec.rb
@@ -18,43 +18,32 @@ RSpec.describe 'Transforming Yale Arabic and Persian Medical Records CSV file' d
     indexer.process(data)
     expect(CreateResourceJob).to have_received(:perform_later) do |_id, _two, json|
       dlme = JSON.parse(json)
-      expect(dlme['agg_provider']).to eq 'Yale Univerity Libraries'
+      expect(dlme['agg_provider']).to eq 'Yale University Libraries'
       expect(dlme['id']).to eq 'yale_4021741'
-      expect(dlme['cho_id']).to eq 'yale_4021741'
-      expect(dlme['cho_provenance']).to eq 'Venice'
-      expect(dlme['cho_creator']).to eq [
-        'AverroeÌˆs, 1126-1198. Colliget',
-        'Ibn Zuhr, Ê»Abd al-Malik ibn AbiÌ„ al-Ê»AlaÌ„Ê¼, d. 1162',
-        'Jacobus Hebraeus, 13th cent', 'Magister Patavinus, 13th cent'
+      expect(dlme['cho_identifier']).to eq ['4021741']
+      expect(dlme['cho_provenance']).to eq [
+        'Venice',
+        'Ellenbog, Ulrich,--ca. 1435-1499.--Ownership'
       ]
-      expect(dlme['cho_title']).to eq 'Liber Teisir, sive Rectificatio medicationis et regiminis : antidotarium.'
-      expect(dlme['cho_alternative_title']).to eq 'Liber Teisir, sive Rectificatio medicationis et regiminis : antidotarium.'
-      expect(dlme['cho_publisher']).to eq 'Joannes and Gregorius de Gregoriis, de Forlivio'
-      expect(dlme['edm:dateProvide']).to eq '4 January 1490-1491'
-      expect(dlme['cho_description']).to eq '[4], 40, 63, [1] leaves ; 32 cm.'
-      expect(dlme['cho_language']).to eq 'Latin'
-      expect(dlme['cho_description']).to eq [
-        'Includes Averroes: Colliget.',
-        'Translated from Arabic into Hebrew by Jacobus Hebraeus; into Latin by Paravicius.',
-        'Yale Med copy from Ulrich Ellenbog, who signed, annotated, and decorated the text with some red and blue letters.',
-        'Yale Med copy has contemporary German binding, K109 Salve-Meister, Memmingen. Described by Scott Husby, 2010.'
-      ]
-      expect(dlme['cho_description']).to eq nil
-      expect(dlme['cho_provenance']).to eq 'Ellenbog, Ulrich,--ca. 1435-1499.--Ownership'
+      expect(dlme['cho_title']).to eq ['Liber Teisir, sive Rectificatio medicationis et regiminis : antidotarium.']
+      expect(dlme['cho_alternative']).to eq ['Rectificatio medicationis et regiminis']
+      expect(dlme['cho_publisher']).to eq ['Joannes and Gregorius de Gregoriis, de Forlivio']
+      expect(dlme['edm:dateProvider']).to eq ['4 January 1490-1491']
+      expect(dlme['cho_description']).to include('[4], 40, 63, [1] leaves ; 32 cm.')
+      expect(dlme['cho_language']).to eq ['Latin']
       expect(dlme['cho_subject']).to eq [
         'Incunabula in Yale Library',
         'Materia medica',
         'Medicine, Arab',
         'Therapeutics--Early works to 1800'
       ]
-      expect(dlme['cho_edm_type']).to eq 'text'
-      expect(dlme['cho_is_part_of']).to eq 'Cushing/Whitney Medical Library'
-      expect(dlme['wr_dc_rights']).to start_with 'The use of this image may be subject to the copyright law of the United States'\
-      ' (Title 17, United States Code) or to site license or other rights management terms and conditions. The person using the'\
-      ' image is liable for any infringement'
-      expect(dlme['cho_provenance']).to eq 'Arcadia Fund, 2013'
-      expect(dlme['agg_is_shown_by']).to eq 'http://hdl.handle.net/10079/bibid/4021741'
-      expect(dlme['agg_is_shown_at']).to eq 'http://hdl.handle.net/10079/bibid/4021741'
+      expect(dlme['cho_edm_type']).to eq ['text']
+      expect(dlme['cho_is_part_of']).to eq ['Cushing/Whitney Medical Library']
+      expect(dlme['wr_dc_rights']).to eq ['The use of this image may be subject to the copyright law of the United States (Title'\
+      ' 17, United States Code) or to site license or other rights management terms and conditions. The person using the image'\
+      ' is liable for any infringement']
+      expect(dlme['agg_is_shown_by']).to include('wr_id' => 'http://hdl.handle.net/10079/digcoll/22466')
+      expect(dlme['agg_is_shown_at']).to include('wr_id' => 'http://hdl.handle.net/10079/digcoll/22466')
     end
   end
 end
