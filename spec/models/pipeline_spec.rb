b3b952937baf76aa8000f0f3c9f6377f63ff49dd
Justin Coyne <jcoyne@justincoyne.com>
2019-02-11 15:30:59 -0600

    Remove traject
    This code has been moved to other repositories:
    * https://github.com/sul-dlss/dlme-traject
    * ttps://github.com/sul-dlss/dlme-transform/pull/13

--- spec/models/pipeline_spec.rb
+++ spec/models/pipeline_spec.rb
@@ -3,22 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe Pipeline, type: :model do
-  describe '#indexer' do
-    subject(:indexer) { described_class.for('stanford_mods').indexer(resource) }
-
-    let(:resource) { create(:harvested_resource, original_filename: 'x.csv') }
-
-    it 'creates a new traject indexer for a resource' do
-      expect(indexer).to be_a_kind_of Traject::Indexer
-    end
-
-    it 'sets the original filename from the provided resource' do
-      expect(indexer.settings['command_line.filename']).to eq 'x.csv'
-    end
-
-    it 'sets the harvest context' do
-      expect(indexer.settings['harvested_resource_id']).to eq resource.id
-      expect(indexer.settings['harvest_id']).to eq resource.harvest.id
-    end
-  end
+  it 'will have tests'
 end
