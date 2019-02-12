46d85ccd85b2ad1079bf1e4a66b78b92fe89b33f
Justin Coyne <jcoyne@justincoyne.com>
2019-02-12 11:50:40 -0600

    The application should not be dependent on a database at startup
    This can occur if the app starts up ahead of the database or if we just want to compile assets
    
    See: https://github.com/projectblacklight/spotlight/issues/2133

--- config/initializers/translations.rb
+++ config/initializers/translations.rb
@@ -2,7 +2,9 @@ require 'i18n/backend/active_record'
 
 Translation = I18n::Backend::ActiveRecord::Translation
 
-if Translation.table_exists?
+# See https://github.com/projectblacklight/spotlight/issues/2133
+# if Translation.table_exists?
+begin
   ##
   # Sets up the new Spotlight Translation backend, backed by ActiveRecord. To
   # turn on the ActiveRecord backend, uncomment the following lines.
