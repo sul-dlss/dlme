6564b455ee1b34b3d1a2169639a1b4019804d00f
aaron-collier <aaron.collier@stanford.edu>
2019-02-25 14:09:44 -0800

    Set path and ID appropriately for redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 if defined? Sidekiq
   Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://:#{ENV['REDIS_PASS']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/1" }
+    config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/1", id: "0001" }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://:#{ENV['REDIS_PASS']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/1" }
+    config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/1", id: "0001" }
   end
 end
