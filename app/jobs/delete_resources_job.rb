7a6bf997cc1af97e604eea4406e785ea35ad7bee
Justin Coyne <jcoyne@justincoyne.com>
2019-09-24 09:33:44 -0500

    Ensure the record is found before operating on it
    Fixes #566

--- app/jobs/delete_resources_job.rb
+++ app/jobs/delete_resources_job.rb
@@ -8,17 +8,19 @@ class DeleteResourcesJob < ApplicationJob
     resp = Faraday.get(url)
 
     resource = resp.body.split("\n")
-    resource.each_with_index do |item, index|
-      remove_resource(item, exhibit, index)
+    resource.each do |json|
+      remove_resource(json: json, exhibit: exhibit)
     end
     logger.info("#{resource.count} records were removed from #{url}.")
   end
 
   private
 
-  def remove_resource(item, exhibit, _index)
-    json = JSON.parse(item)
-    resource = DlmeJson.find_by(url: json['id'], exhibit: exhibit)
+  def remove_resource(json:, exhibit:)
+    item = JSON.parse(json)
+    resource = DlmeJson.find_by(url: item['id'], exhibit: exhibit)
+    return unless resource
+
     # This approach will work for DLME where we only have one exhibit, but
     # will need to be made more complicated for Spotlight in general because
     # one resource could be part of more than one exhibit.
--- spec/jobs/delete_resources_job_spec.rb
+++ spec/jobs/delete_resources_job_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe DeleteResourcesJob, type: :job do
+  subject(:perform) { described_class.perform_now(url, exhibit) }
+
   let(:exhibit) { create(:exhibit) }
-  let!(:resource) { create(:dlme_json) }
 
   let(:url) { 'http://s3/mybucket/myfile.json' }
 
@@ -18,13 +19,32 @@ RSpec.describe DeleteResourcesJob, type: :job do
   end
 
   before do
-    resource.reindex
-    allow(DlmeJson).to receive(:find_by).and_return(resource)
     allow(Faraday).to receive(:get).and_return(mock_response)
+    allow(Rails.logger).to receive(:info)
   end
 
-  it 'deletes resources' do
-    expect { described_class.perform_now(url, exhibit) }
-      .to change(DlmeJson, :count).by(-1)
+  context 'when a resource is found' do
+    let!(:resource) { create(:dlme_json) }
+
+    before do
+      resource.reindex
+      allow(DlmeJson).to receive(:find_by).and_return(resource)
+    end
+
+    it 'deletes resources' do
+      expect { perform }.to change(DlmeJson, :count).by(-1)
+      expect(Rails.logger).to have_received(:info).with('1 records were removed from http://s3/mybucket/myfile.json.')
+    end
+  end
+
+  context 'when a resource is not found' do
+    before do
+      allow(DlmeJson).to receive(:find_by).and_return(nil)
+    end
+
+    it "doesn't raise any error" do
+      perform
+      expect(Rails.logger).to have_received(:info).with('1 records were removed from http://s3/mybucket/myfile.json.')
+    end
   end
 end
