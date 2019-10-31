62bc5c00b10e83beff92fe57cfab166fb82ed9ea
Jessie Keck <jessie.keck@gmail.com>
2019-10-30 17:02:43 -0700

    Install (selenium)-webdriver(s).

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,9 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require_relative 'support/controller_level_helpers'
+require 'selenium-webdriver'
+
+Capybara.javascript_driver = :selenium_chrome_headless
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
