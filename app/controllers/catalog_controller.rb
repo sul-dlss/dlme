1268b0c2b1921da89030231e2bc79755a940d3d9
Chris Beer <cabeer@stanford.edu>
2019-11-08 12:51:56 -0800

    Take a stab at supporting mixed language metadata as per #589

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -39,20 +39,44 @@ class CatalogController < ApplicationController
       end.to_h
     end
 
+    lang_config = {
+      'ar' => [%w[ar-Arab ar-Latn], default: (%w[en none] + Settings.acceptable_bcp47_codes).uniq],
+      'en' => ['en', default: (%w[ar-Arab ar-Latn none] + Settings.acceptable_bcp47_codes).uniq]
+    }.with_indifferent_access
+
     multilingual_locale_aware_field = lambda do |field_prefix, suffix = 'ssim', &block|
-      langs = Settings.acceptable_bcp47_codes + ['none']
-      block.yield({
-        pattern: "#{field_prefix}.%{lang}_#{suffix}",
-        langs: langs,
-        values: lambda do |field_config, document|
-          field_config.langs.flat_map do |lang|
-            Blacklight::FieldRetriever.new(document, field_config.merge(field: field_config.pattern % { lang: lang }, values: nil)).fetch
+      block.yield(
+        {
+          pattern: "#{field_prefix}.%<lang>s_#{suffix}",
+          values: lambda do |field_config, document|
+            pref_langs, options = lang_config[I18n.locale]
+
+            values = Array.wrap(pref_langs).flatten.map do |lang|
+              subfield_config = field_config.merge(field: format(field_config.pattern, lang: lang), values: nil)
+              Blacklight::FieldRetriever.new(document, subfield_config).fetch
+            end
+
+            if values.none?(&:any?)
+              values = Array.wrap(options[:default]).flatten.map do |lang|
+                subfield_config = field_config.merge(field: format(field_config.pattern, lang: lang), values: nil)
+                Blacklight::FieldRetriever.new(document, subfield_config).fetch
+              end
+            end
+
+            if field_config.first
+              values.find(&:any?)
+            else
+              values.flatten
+            end
           end
-        end
-      })
+        }
+      )
     end
 
-    config.index.title_field = locale_encoded_fields.call('cho_title').values
+    multilingual_locale_aware_field.call('cho_title') do |field_config|
+      config.index.title_field = Blacklight::Configuration::Field.new(first: true, **field_config)
+    end
+
     config.index.thumbnail_field = 'agg_preview.wr_id_ssim'
     config.index.default_thumbnail = 'default.png'
 
