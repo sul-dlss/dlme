2db3ca0c317b94bac21827d04bd0e00639dd57b0
Jessie Keck <jessie.keck@gmail.com>
2019-11-11 11:50:56 -0800

    Add an Items section to the statistics dashboard.

--- spec/features/statistics_spec.rb
+++ spec/features/statistics_spec.rb
@@ -3,7 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Statistics page', type: :feature do
-  before { create(:exhibit) }
+  let(:stub_response) do
+    { 'response' => { 'numFound' => 100 } }
+  end
+  let(:stub_dashboard) do
+    StatisticsDashboard.new(search_service: instance_double(
+      'SearchService',
+      search_results: [stub_response]
+    ))
+  end
+
+  before do
+    create(:exhibit)
+    allow(StatisticsDashboard).to receive(:new).and_return(stub_dashboard)
+  end
 
   it 'has a page available to users via a menu item in the exhibit navbar' do
     visit root_path
@@ -14,5 +27,15 @@ RSpec.describe 'Statistics page', type: :feature do
 
     expect(page).to have_css('.exhibit-navbar li.nav-item.active', text: 'Statistics')
     expect(page).to have_css('h1', text: 'Statistics')
+  end
+
+  it 'has an items section' do
+    visit root_path
+
+    within '.exhibit-navbar' do
+      click_link 'Statistics'
+    end
+
+    expect(page).to have_css('h2', text: 'Items · 100')
   end
 end
---
+++ spec/views/statistics/_items.html.erb_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'statistics/_items.html.erb', type: :view do
+  before do
+    controller.singleton_class.class_eval do
+      include Blacklight::Controller
+    end
+
+    render partial: 'statistics/items', locals: {
+      items: items
+    }
+  end
+
+  let(:stub_response) do
+    { 'facet_counts' => {
+      'facet_fields' => {
+        'cho_language.en_ssim' => ['Value 1', '500', 'Value 2', '300']
+      },
+      'facet_pivot' => {
+        'cho_edm_type.en_ssim,cho_has_type.en_ssim' => [
+          { 'value' => 'Value 1', 'count' => '500' },
+          { 'value' => 'Value 2', 'count' => '300', 'pivot' => [
+            { 'value' => 'Value A', 'count' => '200' },
+            { 'value' => 'Value B', 'count' => '100' }
+          ] }
+        ]
+      }
+    } }
+  end
+
+  let(:items) do
+    StatisticsDashboard::Items.new(
+      instance_double(
+        'SearchService',
+        search_results: [stub_response]
+      )
+    )
+  end
+
+  describe 'By type section' do
+    it 'has a table with linked langage values and their counts' do
+      expect(rendered).to have_css('table.by-type')
+      expect(rendered).to have_css('table.by-type tr a', text: 'Value 1')
+      expect(rendered).to have_css('table.by-type tr a', text: 'Value 2')
+      expect(rendered).to have_css('table.by-type tr td', text: '500')
+      expect(rendered).to have_css('table.by-type tr td', text: '300')
+    end
+
+    it 'includes a first level of pivot facets (indented)' do
+      expect(rendered).to have_css('table.by-type tr td.pl-5 a', text: 'Value A')
+      expect(rendered).to have_css('table.by-type tr td.pl-5 a', text: 'Value B')
+      expect(rendered).to have_css('table.by-type tr td', text: '200')
+      expect(rendered).to have_css('table.by-type tr td', text: '100')
+    end
+  end
+
+  describe 'By language section' do
+    it 'has a table with linked language values and their counts' do
+      expect(rendered).to have_css('table.by-language')
+      expect(rendered).to have_css('table.by-language tr a', text: 'Value 1')
+      expect(rendered).to have_css('table.by-language tr a', text: 'Value 2')
+      expect(rendered).to have_css('table.by-language tr td', text: '500')
+      expect(rendered).to have_css('table.by-language tr td', text: '300')
+    end
+  end
+end
