f5de6c7076cea86c169ce9c79bf8752723db8a66
Chris Beer <cabeer@stanford.edu>
2019-11-18 14:16:22 -0800

    Display both Gregorian and Hijri dates on results and show pages; fixees #693

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -29,4 +29,30 @@ RSpec.describe ApplicationHelper, type: :helper do
       expect(links).to have_link('Interview', href: /\?f%5Bcho_type_facet%5D%5B%5D=Sound%3AInterview&/)
     end
   end
+
+  describe '#display_date_ranges' do
+    it 'is nil if there is no data' do
+      expect(helper.display_date_ranges(values: [])).to be_nil
+    end
+
+    it 'displays both Gregorian and Hijri data' do
+      actual = helper.display_date_ranges(values: [{ gregorian: [1995], hijri: [1415, 1416] }])
+      expect(actual).to eq '1995 (Gregorian) / 1415 - 1416 (Hijri)'
+    end
+
+    it 'rolls up ranges, but only if they are consecutive' do
+      actual = helper.display_date_ranges(values: [{ gregorian: [1994, 1995, 1997], hijri: [1414, 1415, 1416, 1417, 1418] }])
+      expect(actual).to eq '1994 - 1995 and 1997 (Gregorian) / 1414 - 1418 (Hijri)'
+    end
+
+    it 'still works if there is only hijri data' do
+      actual = helper.display_date_ranges(values: [{ hijri: [1415, 1416] }])
+      expect(actual).to eq '1415 - 1416 (Hijri)'
+    end
+
+    it 'still works if there is only gregorian data' do
+      actual = helper.display_date_ranges(values: [{ gregorian: [1995] }])
+      expect(actual).to eq '1995 (Gregorian)'
+    end
+  end
 end
