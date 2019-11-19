b5d9e5862b180739d50efb1a3bfed868780855c8
Jessie Keck <jessie.keck@gmail.com>
2019-11-19 14:57:18 -0800

    Add the Contributors jumbotron section to the top of the statistics dashboard.

--- spec/features/statistics_spec.rb
+++ spec/features/statistics_spec.rb
@@ -4,7 +4,21 @@ require 'rails_helper'
 
 RSpec.describe 'Statistics page', type: :feature do
   let(:stub_response) do
-    { 'response' => { 'numFound' => 100 } }
+    {
+      'response' => { 'numFound' => 100 },
+      'facet_counts' => {
+        'facet_pivot' => {
+          'agg_provider.en_ssim,agg_provider_country.en_ssim' => [
+            { 'value' => 'Institution 1', 'count' => '500', 'pivot' => [
+              { 'value' => 'Country 1', 'count' => '500' }
+            ] },
+            { 'value' => 'Institution 2', 'count' => '300', 'pivot' => [
+              { 'value' => 'Country 2', 'count' => '300' }
+            ] }
+          ]
+        }
+      }
+    }
   end
   let(:stub_dashboard) do
     StatisticsDashboard.new(search_service: instance_double(
@@ -16,26 +30,27 @@ RSpec.describe 'Statistics page', type: :feature do
   before do
     create(:exhibit)
     allow(StatisticsDashboard).to receive(:new).and_return(stub_dashboard)
-  end
 
-  it 'has a page available to users via a menu item in the exhibit navbar' do
     visit root_path
 
     within '.exhibit-navbar' do
       click_link 'Statistics'
     end
+  end
 
+  it 'has a page available to users via a menu item in the exhibit navbar' do
     expect(page).to have_css('.exhibit-navbar li.nav-item.active', text: 'Statistics')
     expect(page).to have_css('h1', text: 'Statistics')
   end
 
   it 'has an items section' do
-    visit root_path
-
-    within '.exhibit-navbar' do
-      click_link 'Statistics'
-    end
-
+    expect(page).to have_css('.jumbotron h2', text: '100 items')
     expect(page).to have_css('h2', text: 'Items · 100')
+  end
+
+  it 'has a Contributors section' do
+    expect(page).to have_css('.jumbotron h2', text: '2 contributors')
+    expect(page).to have_css('.jumbotron p', text: '2 countries')
+    expect(page).to have_css('h2', text: 'Contributors · 2')
   end
 end
