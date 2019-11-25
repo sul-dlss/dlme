4a3f9981afa3f3fb48a3abecf64db78ce29e4713
Jack Reed <phillipjreed@gmail.com>
2019-11-25 07:41:20 -0700

    Pass Factory class as string to appease the 🚓

--- spec/factories/dlme_jsons.rb
+++ spec/factories/dlme_jsons.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :dlme_json, class: DlmeJson do
+  factory :dlme_json, class: 'DlmeJson' do
     data do
       { json: '{"id":"test_id", "agg_provider":"factorybot", ' \
                '"agg_data_provider":"factorybot", "cho_title":["test object"]}' }
