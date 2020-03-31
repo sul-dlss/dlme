9f3e24723dd82c5df4b1a4c7419a4d03b983f27f
Chris Beer <cabeer@stanford.edu>
2020-03-31 07:22:30 -0700

    Authorize range limit actions from exhibit home pages
    Fixes #850

--- app/controllers/spotlight/home_pages_controller.rb
+++ app/controllers/spotlight/home_pages_controller.rb
@@ -10,5 +10,15 @@ module Spotlight
   # Override the upstream HomePagesController in order to inject range limit behaviors
   class HomePagesController
     include BlacklightRangeLimit::ControllerOverride
+
+    # rubocop:disable Rails/LexicallyScopedActionFilter
+    # Tweak the authorization for the range limit actions
+    before_action :authenticate_user!, except: %i[show range_limit range_limit_panel]
+    skip_authorize_resource only: %i[range_limit range_limit_panel]
+
+    before_action only: %i[range_limit range_limit_panel] do
+      authorize! :read, @page
+    end
+    # rubocop:enable Rails/LexicallyScopedActionFilter
   end
 end
