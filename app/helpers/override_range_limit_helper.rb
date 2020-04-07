69889a37a6f07a490606463c014247e53ac14d9f
Jack Reed <phillipjreed@gmail.com>
2020-04-07 13:41:45 -0600

    Setup date range suffixes to be internationalized for Arabic use also

--- app/helpers/override_range_limit_helper.rb
+++ app/helpers/override_range_limit_helper.rb
@@ -10,14 +10,18 @@ module OverrideRangeLimitHelper
     display = super
 
     # rubocop:disable Rails/OutputSafety
+    case solr_field
     # Hijiri date
-    return display.gsub(/>(-)(\d+)</, '>\2 BH<').gsub(/>(\d+)</, '>\1 H<').html_safe if solr_field == 'cho_date_range_hijri_isim'
-
+    when 'cho_date_range_hijri_isim'
+      display.gsub(/>(-)(\d+)</, ">\\2 #{I18n.t('date.suffix.bh')}<")
+             .gsub(/>(\d+)</, ">\\1 #{I18n.t('date.suffix.h')}<").html_safe
     # Gregorian date
-    return display.gsub(/>(-)(\d+)</, '>\2 BCE<').gsub(/>(\d+)</, '>\1 CE<').html_safe if solr_field == 'cho_date_range_norm_isim'
-
+    when 'cho_date_range_norm_isim'
+      display.gsub(/>(-)(\d+)</, ">\\2 #{I18n.t('date.suffix.bce')}<")
+             .gsub(/>(\d+)</, ">\\1 #{I18n.t('date.suffix.ce')}<").html_safe
     # rubocop:enable Rails/OutputSafety
-
-    display
+    else
+      display
+    end
   end
 end
