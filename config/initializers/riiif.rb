26ebe17bdec6e91a663db30fcf3c7e9f80f06d28
Jack Reed <phillipjreed@gmail.com>
2021-02-03 15:26:48 -0700

    Wrap riiif config in ActiveSupport::Reloader.to_prepare so that the file_resolver does not reset

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,16 +1,18 @@
-if Settings.s3.upload_bucket
-  Riiif::Image.file_resolver = Riiif::HTTPFileResolver.new
-  Riiif::Image.file_resolver.id_to_uri = lambda do |id|
-    aws_file = Spotlight::FeaturedImage.find(id).image.file
-    raise Riiif::ImageNotFoundError, "unable to find file for #{id}" if aws_file.nil?
+ActiveSupport::Reloader.to_prepare do
+  if Settings.s3.upload_bucket
+    Riiif::Image.file_resolver = Riiif::HTTPFileResolver.new
+    Riiif::Image.file_resolver.id_to_uri = lambda do |id|
+      aws_file = Spotlight::FeaturedImage.find(id).image.file
+      raise Riiif::ImageNotFoundError, "unable to find file for #{id}" if aws_file.nil?
 
-    aws_file.file.presigned_url('GET')
+      aws_file.file.presigned_url('GET')
+    end
+  else
+    Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
   end
-else
-  Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
-end
-# Riiif::Image.authorization_service = IIIFAuthorizationService
+  # Riiif::Image.authorization_service = IIIFAuthorizationService
 
-# Riiif.not_found_image = 'app/assets/images/us_404.svg'
-#
-Riiif::Engine.config.cache_duration = 365.days
+  # Riiif.not_found_image = 'app/assets/images/us_404.svg'
+  #
+  Riiif::Engine.config.cache_duration = 365.days
+end
