1285e9f70daa74ebf5a78f5684d62524c725864d
Aaron Collier <aaron.collier@stanford.edu>
2021-02-24 16:30:47 -0800

    Add an environment variable for the rails server to prepare for a published web image

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-envsubst '$RAILS_ROOT' < /tmp/app.conf > /etc/nginx/conf.d/default.conf
+envsubst '$RAILS_ROOT $RAILS_SERVER' < /tmp/app.conf > /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
