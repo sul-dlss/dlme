43d927eaf11fd5fc8689dc7bff28d20ca689d01c
Chris Beer <chris@cbeer.info>
2021-05-10 06:46:55 -0700

    Update spotlight db migrations

---
+++ db/migrate/20210510134610_create_bulk_updates.spotlight.rb
@@ -0,0 +1,10 @@
+# This migration comes from spotlight (originally 20210305171150)
+class CreateBulkUpdates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :spotlight_bulk_updates do |t|
+      t.string :file, null: false
+      t.references :exhibit
+      t.timestamps
+    end
+  end
+end
