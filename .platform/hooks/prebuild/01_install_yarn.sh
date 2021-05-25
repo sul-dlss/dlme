9fd72f988b6b33e6d17a9bd9b09c37739bfd02eb
Chris Beer <chris@cbeer.info>
2021-05-25 12:48:40 -0700

    Install yarn through npm

--- .platform/hooks/prebuild/01_install_yarn.sh
+++ .platform/hooks/prebuild/01_install_yarn.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-echo "Installing NodeJS and Yarn"
-
-# Install Node
-echo "Checking if Node is already installed"
-if ! command -v node > /dev/null; then
-  echo "Installing Node...";
-  curl --silent --location https://rpm.nodesource.com/setup_12.x | bash -;
-fi
-
-node --version;
+echo "Installing Yarn"
 
 # Install yarn
 echo "Checking if Yarn is already installed"
 if ! command -v yarn > /dev/null; then
-  curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo;
-  sudo yum install -y yarn;
+  npm i -g yarn
+  ln -s "$(npm bin --global)"/yarn /usr/bin/yarn
 fi
 
 yarn --version
