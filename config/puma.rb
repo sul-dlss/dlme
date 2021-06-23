f9058e4aa9a7969c604d490b555e740e83cfe1b5
Chris Beer <chris@cbeer.info>
2021-06-23 10:27:41 -0700

    Increase puma's persistent timeout setting

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,7 @@ threads min_threads_count, max_threads_count
 # terminating a worker in development environments.
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+persistent_timeout 75
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
