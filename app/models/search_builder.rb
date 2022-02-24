8be51d106ad24510db4e1e7ef8f79ae2e65d15b6
Marlo Longley <marlo-longley@users.noreply.github.com>
2022-02-24 11:00:09 -0800

    Set Solr min match parameter to 1 on boolean search (#1493)
    Co-authored-by: Cory Lown <corylown@stanford.edu>

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+##
+# Blacklight’s SearchBuilder converts blacklight request parameters into query parameters appropriate for search index.
+# It does so by evaluating a chain of processing methods to populate a result hash
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightRangeLimit::RangeLimitBuilder
@@ -12,4 +15,18 @@ class SearchBuilder < Blacklight::SearchBuilder
   #   def add_custom_data_to_query(solr_parameters)
   #     solr_parameters[:custom] = blacklight_params[:user_value]
   #   end
+
+  self.default_processor_chain += [:min_match_for_boolean]
+
+  ##
+  # Sets `mm` (minimum should match) to 1 based on presence of boolean operators, overriding default set in Solr.
+  # The default value in the request handler (`6<-1 6<90%`) allows for some non-matching clauses in
+  # full-text queries, but leads to unexpected behavior when a user expects to use boolean operators for
+  # more advanced search queries. Setting `mm=1` means only 1 clause needs to match, but the lucene query parser (in
+  # edismax) will precompose the query strings so everything works out.
+  def min_match_for_boolean(solr_parameters)
+    return unless search_state.query_param&.match?(/\s(AND|OR|NOT)\s/)
+
+    solr_parameters[:mm] = '1'
+  end
 end
--- spec/models/search_builder_spec.rb
+++ spec/models/search_builder_spec.rb
@@ -5,17 +5,28 @@ require 'rails_helper'
 RSpec.describe SearchBuilder do
   subject(:search_builder) { described_class.new scope }
 
-  let(:user_params) { {} }
   let(:blacklight_config) { Blacklight::Configuration.new }
   let(:scope) { instance_double ApplicationController, blacklight_config: blacklight_config }
 
-  # describe "my custom step" do
-  #   subject(:query_parameters) do
-  #     search_builder.with(user_params).processed_parameters
-  #   end
-  #
-  #   it "adds my custom data" do
-  #     expect(query_parameters).to include :custom_data
-  #   end
-  # end
+  describe '#min_match_for_boolean' do
+    subject(:solr_parameters) do
+      search_builder.with(query).processed_parameters
+    end
+
+    context 'when query has a boolean operator' do
+      let(:query) { { q: 'jupiter AND (mars OR saturn)' } }
+
+      it 'sets min match to 1' do
+        expect(solr_parameters[:mm]).to eq('1')
+      end
+    end
+
+    context 'when query has no boolean operator' do
+      let(:query) { { q: 'jupiter' } }
+
+      it 'does not set min match' do
+        expect(solr_parameters[:mm]).to be_nil
+      end
+    end
+  end
 end
