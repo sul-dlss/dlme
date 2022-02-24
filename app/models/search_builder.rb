bc2bc21a2360d55ec3e1dadb5d353db560e22a01
Cory Lown <corylown@stanford.edu>
2022-02-24 13:46:50 -0500

    Apply @cbeer review suggestions. Expand mm comment.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -18,12 +18,14 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   self.default_processor_chain += [:min_match_for_boolean]
 
-  # Sets min match to 1 based on presence of boolean operators, overriding default set in Solr
+  ##
+  # Sets `mm` (minimum should match) to 1 based on presence of boolean operators, overriding default set in Solr.
+  # The default value in the request handler (`6<-1 6<90%`) allows for some non-matching clauses in
+  # full-text queries, but leads to unexpected behavior when a user expects to use boolean operators for
+  # more advanced search queries. Setting `mm=1` means only 1 clause needs to match, but the lucene query parser (in
+  # edismax) will precompose the query strings so everything works out.
   def min_match_for_boolean(solr_parameters)
-    return unless blacklight_params &&
-                  blacklight_params[:q].present? &&
-                  blacklight_params[:q].respond_to?(:match) &&
-                  blacklight_params[:q].match(/\s(AND|OR|NOT)\s/)
+    return unless search_state.query_param&.match?(/\s(AND|OR|NOT)\s/)
 
     solr_parameters[:mm] = '1'
   end
