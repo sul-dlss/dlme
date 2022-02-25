33c234442a591407ec66bfb92c6d2bafe3151a9c
Nick Budak <budak@stanford.edu>
2022-02-24 16:58:53 -0800

    Change examples in search page e2e test

--- spec/e2e/search_page_spec.rb
+++ spec/e2e/search_page_spec.rb
@@ -8,20 +8,20 @@ RSpec.describe 'search page', type: :feature, js: true do
   end
 
   it 'can search in arabic' do
-    fill_in('q', with: 'بوس')
+    fill_in('q', with: 'شهرفرنگ')
     click_button(class: 'search-btn')
-    expect(page).to have_content('بؤس العلمانية من بؤس العلمانيين')
+    expect(page).to have_content('لباس زنان در ایران')
   end
 
   it 'can search in english' do
-    fill_in('q', with: 'Astronomical charts')
+    fill_in('q', with: 'Cairo')
     click_button(class: 'search-btn')
-    expect(page).to have_content('Astronomical charts for Fatḥ-ʻAlī Shāh Qājār.')
+    expect(page).to have_content('Cairo University Bridge')
   end
 
   it 'can search in romanized arabic' do
-    fill_in('q', with: 'Būstān.')
+    fill_in('q', with: 'Muṣawwar')
     click_button(class: 'search-btn')
-    expect(page).to have_content('Būstān.')
+    expect(page).to have_content('al-Muṣawwar, No. 27, April 17, 1925')
   end
 end
