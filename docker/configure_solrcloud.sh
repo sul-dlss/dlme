77b7a6938963d5aded416ceed77c341b429dcb50
Nick Budak <budak@stanford.edu>
2022-03-02 10:39:06 -0800

    Update configure_solrcloud to wait for solr to be ready

--- docker/configure_solrcloud.sh
+++ docker/configure_solrcloud.sh
@@ -5,16 +5,30 @@ set -e
 # args & defaults: ./configure_solrcloud.sh <solr_host> <solr_collection>
 SOLR_HOST=${1:-localhost}
 SOLR_COLLECTION=${2:-dlme}  # also used for config name
+SOLR_URL="http://admin:admin@$SOLR_HOST:8983"
+
+# function to wait until receiving a 200 for a given url
+wait_for_ok()
+{
+  while [ "$(curl --silent --location --output /dev/null --write-out '%{http_code}' $1)" != "200" ]; do
+    echo "waiting for solr to be ready..."
+    sleep 1
+  done
+}
 
 # compress solr config so it can be uploaded via http
+echo "Zipping solr config..."
 cd solr/config
 zip -1 -r solr_config.zip ./*
+echo "Created solr_config.zip"
+wait_for_ok $SOLR_URL
 
 # upload the config via the config api
-curl -H "Content-type: application/octet-stream" --data-binary @solr_config.zip "http://admin:admin@$SOLR_HOST:8983/solr/admin/configs?action=UPLOAD&name=$SOLR_COLLECTION"
-
-# wait a bit for config to be ready
-sleep 10
+curl -H "Content-type: application/octet-stream" --data-binary @solr_config.zip "$SOLR_URL/solr/admin/configs?action=UPLOAD&name=$SOLR_COLLECTION"
+echo "Uploaded solr config"
+wait_for_ok $SOLR_URL
 
 # create a new collection using the config
-curl -H "Content-type: application/json" "http://admin:admin@$SOLR_HOST:8983/api/collections/" -d "{create: {name: $SOLR_COLLECTION, config: $SOLR_COLLECTION, numShards: 1}}"
+curl -H "Content-type: application/json" "$SOLR_URL/api/collections/" -d "{create: {name: $SOLR_COLLECTION, config: $SOLR_COLLECTION, numShards: 1}}"
+echo "Created solr collection"
+wait_for_ok $SOLR_URL
