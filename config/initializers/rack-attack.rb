d032509e2a0a413fca35ff80df6d1c197c2a7b28
Chris Beer <chris@cbeer.info>
2022-03-04 10:41:20 -0800

    Add the recommended rack-attack headers for well-behaved clients

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -14,3 +14,18 @@ Rack::Attack.throttle("searches/ip", limit: 15, period: 15.minutes) do |req|
 
   req.ip
 end
+
+Rack::Attack.throttled_response_retry_after_header = true
+
+Rack::Attack.throttled_response = lambda do |env|
+  match_data = env['rack.attack.match_data']
+  now = match_data[:epoch_time]
+
+  headers = {
+    'RateLimit-Limit' => match_data[:limit].to_s,
+    'RateLimit-Remaining' => '0',
+    'RateLimit-Reset' => (now + (match_data[:period] - now % match_data[:period])).to_s
+  }
+
+  [ 429, headers, ["Throttled\n"]]
+end
