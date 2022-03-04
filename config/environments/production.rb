aceef007b0fb8cb970fce0cee4b368d82f24d7b4
Chris Beer <cabeer@stanford.edu>
2022-03-04 09:16:06 -0800

    Disable lograge compact logging
    It removes too much information and makes the logs harder to investigate.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,5 +119,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.lograge.enabled = true
+  config.lograge.enabled = false
 end
