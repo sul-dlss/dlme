4fc636c84c8e3b4c810280cb83c7797928380eea
Aaron Collier <aaron.collier@stanford.edu>
2022-03-09 09:43:11 -0800

    Update docker/invoke_fetch_s3.sh
    Co-authored-by: Chris Beer <cabeer@stanford.edu>

--- docker/invoke_fetch_s3.sh
+++ docker/invoke_fetch_s3.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-
+# This script is called by the ECS task `dlme_index_from_s3`
+# See https://github.com/sul-dlss/terraform-aws/blob/master/organizations/development/dlme/dlme-tasks.tf#L85
 echo "Fetching resources from: ${S3_FETCH_URL}"
 
 bundle exec honeybadger exec rake resources:fetch[$S3_FETCH_URL]
