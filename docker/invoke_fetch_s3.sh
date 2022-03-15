ea3128fbf60d8548d1bee6e9c1f2eba188884d72
Aaron Collier <aaron.collier@stanford.edu>
2022-03-15 09:19:37 -0700

    Add trace to index rake command for debugging

--- docker/invoke_fetch_s3.sh
+++ docker/invoke_fetch_s3.sh
@@ -4,4 +4,5 @@
 # See https://github.com/sul-dlss/terraform-aws/blob/master/organizations/development/dlme/dlme-tasks.tf#L85
 echo "Fetching resources from: ${S3_FETCH_URL}"
 
-bundle exec honeybadger exec rake resources:fetch[$S3_FETCH_URL]
+# Add additional logging output on rake for debugging
+bundle exec honeybadger exec rake resources:fetch[$S3_FETCH_URL] --trace
