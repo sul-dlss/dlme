e374229f4dd78b20f66a8b6aa1b3f7defc3be94d
Nick Budak <budak@stanford.edu>
2023-02-09 11:28:26 -0800

    Add redis check to okcomputer
    Also removes the git version check, which we added for additional
    visibility into the built docker images. No longer necessary for
    non-docker on-prem deployments.
    
    See #1607

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -2,4 +2,4 @@ OkComputer.mount_at = 'status'
 
 OkComputer::Registry.register 'solr', OkComputer::SolrCheck.new(Blacklight.default_index.connection.uri.to_s.sub(/\/$/, ''))
 
-OkComputer::Registry.register 'version', OkComputer::AppVersionCheck.new(env: 'GIT_INFO')
+OKComputer::Registry.register 'redis', OkComputer::RedisCheck.new(url: ENV.fetch('SIDEKIQ_REDIS_URL', 'redis://localhost:6379/'))
