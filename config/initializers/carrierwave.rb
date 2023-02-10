1bba59b8dc1cc5ca4f031490fff97bcd2c4a04aa
Huda Khan <hudakhan@stanford.edu>
2023-02-10 16:23:52 -0500

    Removing S3 handling

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-if Settings.s3.upload_bucket
-  CarrierWave.configure do |config|
-    config.storage = :aws
-    config.aws_bucket = Settings.s3.upload_bucket
-    config.aws_acl = 'bucket-owner-full-control'
-  end
-
-  Spotlight::Engine.config.uploader_storage = :aws
-end
