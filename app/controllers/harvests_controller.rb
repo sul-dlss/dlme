20ca5e54031f77aa9b684cccf038813e7cd4e4ab
Huda Khan <hudakhan@stanford.edu>
2023-03-02 11:26:18 -0500

    adding tests and updating error messages for harvest upload

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -10,10 +10,9 @@ class HarvestsController < ApplicationController
 
   def create
     authorize! :create, DlmeJson
-
+    
     upload = NdjsonUpload.new(params.require(:url))
-
-    return invalid_file unless upload.valid?
+    return handle_error(upload.error) unless upload.valid?
 
     AddResourcesJob.perform_later upload.filepath, exhibit: current_exhibit, local: true
 
@@ -21,7 +20,21 @@ class HarvestsController < ApplicationController
                 notice: t('spotlight.resources.fetch.queued')
   end
 
-  def invalid_file
-    redirect_to spotlight.new_exhibit_resource_path(current_exhibit), flash: { error: t('dlme_s3s.form.error') }
+  def handle_error(error)
+    error_message = error_message(error)
+    redirect_to spotlight.new_exhibit_resource_path(current_exhibit), flash: { error: error_message }
   end
+
+  def error_message(error)
+    #NdjsonUpload can also return a ":no_url" error, but the harvest controller requires the url, so we do not need to check for that condition
+    case error
+    when :duplicate_ids
+      t('dlme_s3s.form.error_duplicate_ids') 
+    when :file_not_found
+      t('dlme_s3s.form.error_file_not_found') 
+    else
+      t('dlme_s3s.form.error_unknown') 
+    end
+  end
+
 end
