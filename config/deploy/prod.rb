fa54fef01217f3c151a9b78f80c62c8c7dce0df8
Nick Budak <budak@stanford.edu>
2023-03-22 11:04:57 -0700

    Make review environment part of production

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,6 +2,8 @@
 
 server 'dlme-prod-a.stanford.edu', user: 'dlme', roles: %w[web db app]
 server 'dlme-prod-b.stanford.edu', user: 'dlme', roles: %w[web app]
+server 'dlme-review-prod-a.stanford.edu', user: 'dlme', roles: %w[web db app]
+server 'dlme-review-prod-b.stanford.edu', user: 'dlme', roles: %w[web app]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
