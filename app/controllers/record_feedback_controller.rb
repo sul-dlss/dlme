622e8e959f19902aa6a671a57dc3cd041ea4b32b
Steve Taylor <sctaylor@stanford.edu>
2024-06-18 08:36:28 -0700

    Add reCAPTCHA to feedback form

--- app/controllers/record_feedback_controller.rb
+++ app/controllers/record_feedback_controller.rb
@@ -5,17 +5,19 @@
 # We're overriding to build from our RecordFeedbackForm class and
 # to redirect to the correct place and set the correct notice
 class RecordFeedbackController < Spotlight::ContactFormsController
-  def create
-    if @contact_form.valid?
-      Spotlight::ContactMailer.report_problem(@contact_form).deliver_now
+  def action
+    'record_feedback'
+  end
 
-      redirect_back(
-        fallback_location: spotlight.exhibit_solr_document_path(current_exhibit),
-        notice: t(:'helpers.submit.record_feedback.created')
-      )
-    else
-      render 'new'
-    end
+  def send_feedback
+    Spotlight::ContactMailer.report_problem(@contact_form).deliver_now
+    redirect_back fallback_location: spotlight.exhibit_solr_document_path(current_exhibit),
+                  notice: t(:'helpers.submit.record_feedback.created')
+  end
+
+  def report_failure
+    redirect_back fallback_location: spotlight.exhibit_solr_document_path(current_exhibit),
+                  alert: t(:'helpers.submit.record_feedback.error')
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,10 @@ module Dlme
     unless Rails.env.production?
       config.slowpoke.timeout = 60
     end
+
+    Recaptcha.configure do |config|
+      config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY', '6Lc6BAAAAAAAAChqRbQZcn_yyyyyyyyyyyyyyyyy')
+      config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY', '6Lc6BAAAAAAAAKN3DRm6VA_xxxxxxxxxxxxxxxxx')
+    end
   end
 end
