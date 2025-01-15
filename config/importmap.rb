577ac68efb1a4cf6d0ffc3efd9e9c513597179ac
Steve Taylor <sctaylor@stanford.edu>
2025-01-15 13:14:56 -0800

    Update jQuery pin

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@4.6.2/dist/js/bootstrap.js"
 pin "popper.js", to: "https://ga.jspm.io/npm:popper.js@1.16.1/dist/umd/popper.js"
 pin "i18n-js", to: "https://ga.jspm.io/npm:i18n-js@4.2.3/dist/import/index.js"
