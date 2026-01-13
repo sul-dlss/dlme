2fb6dc1f23cebdae8ad6ea69e96e26ea9a04492b
Justin Coyne <jcoyne@justincoyne.com>
2026-01-13 09:19:05 -0600

    Upgrade spotlight and all dependencies

--- config/webpack/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const { webpackConfig } = require('shakapacker')
-
-// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-
-module.exports = webpackConfig
