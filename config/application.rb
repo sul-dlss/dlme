345bd072215d502ae1fb9c72be805dede508e0eb
Steve Taylor <sctaylor@stanford.edu>
2026-09-15 14:49:31 -0700

    Disable Active Storage variants

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module Dlme
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    # We don't use Active Storage variants.
+    # Disabling dodges some libvips checks.
+    config.active_storage.variant_processor = :disabled
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
